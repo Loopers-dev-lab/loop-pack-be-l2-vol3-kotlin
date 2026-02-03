a7adc8795ec710c908688303d65ed97caa2c837c
이준석 <dlwnstjr0310@gmail.com>
2026-02-03 21:58:45 +0900

    feat: 회원가입 서비스 구현

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/user/UserService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/user/UserService.kt
@@ -1,5 +1,7 @@
 package com.loopers.domain.user
 
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
 import org.springframework.stereotype.Component
 
 @Component
@@ -8,6 +10,9 @@ class UserService(
 ) {
 
     fun signUp(command: UserCommand.SignUp): User {
+        if (userRepository.existsByLoginId(command.loginId)) {
+            throw CoreException(ErrorType.CONFLICT, "이미 존재하는 로그인 ID입니다.")
+        }
         return userRepository.save(command.toEntity())
     }
 }
