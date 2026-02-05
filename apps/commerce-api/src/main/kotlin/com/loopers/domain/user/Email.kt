926815c7b91e82d66c06595ed0c4cd1f720277dc
eoruadl <108312272+eoruadl@users.noreply.github.com>
2026-02-06 02:14:49 +0900

    feat: Email Value Object 구현
    - 이메일 형식 검증
    - JPA AttributeConverter 구현

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/user/Email.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/user/Email.kt
@@ -1,6 +1,6 @@
 package com.loopers.domain.user
 
-data class Email (val value: String) {
+data class Email(val value: String) {
     init {
         require(value.matches(Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$"))) {
             "Email 형식(****@**.**)을 만족해야합니다."
--- apps/commerce-api/src/test/kotlin/com/loopers/domain/user/EmailTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/EmailTest.kt
@@ -7,32 +7,28 @@ import org.junit.jupiter.api.assertThrows
 class EmailTest {
 
     @Test
-    fun `이메일 형식을 만족하면 통과한다` () {
-
+    fun `이메일 형식을 만족하면 통과한다`() {
         assertDoesNotThrow {
             Email("test1234@loopers.com")
         }
     }
 
     @Test
-    fun `@가 없으면 실패한다` () {
-
+    fun `@가 없으면 실패한다`() {
         assertThrows<IllegalArgumentException> {
             Email("test1234loopers.com")
         }
     }
 
     @Test
-    fun `도메인이 없으면 실패한다` () {
-
+    fun `도메인이 없으면 실패한다`() {
         assertThrows<IllegalArgumentException> {
             Email("test1234@")
         }
     }
 
     @Test
-    fun `빈 값이면 실패한다` () {
-
+    fun `빈 값이면 실패한다`() {
         assertThrows<IllegalArgumentException> {
             Email("")
         }
