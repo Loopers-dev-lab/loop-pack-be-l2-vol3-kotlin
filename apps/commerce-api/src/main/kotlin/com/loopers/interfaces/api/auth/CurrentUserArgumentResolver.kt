1f32b14cc7c4daf18f1b42898bc39b528d8f201a
yewonahn <yewonahn0930@gmail.com>
2026-02-05 13:08:23 +0900

    feat: CurrentUser ArgumentResolver 구현

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/auth/CurrentUserArgumentResolver.kt
@@ -0,0 +1,42 @@
+package com.loopers.interfaces.api.auth
+
+import com.loopers.domain.user.User
+import com.loopers.domain.user.UserAuthService
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.UserErrorCode
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Component
+class CurrentUserArgumentResolver(
+    private val userAuthService: UserAuthService,
+) : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(CurrentUser::class.java) &&
+            parameter.parameterType == User::class.java
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): User {
+        val loginId = webRequest.getHeader(HEADER_LOGIN_ID)
+            ?: throw CoreException(UserErrorCode.AUTHENTICATION_FAILED)
+        val password = webRequest.getHeader(HEADER_LOGIN_PW)
+            ?: throw CoreException(UserErrorCode.AUTHENTICATION_FAILED)
+
+        return userAuthService.authenticate(loginId, password)
+    }
+
+    companion object {
+        private const val HEADER_LOGIN_ID = "X-Loopers-LoginId"
+        private const val HEADER_LOGIN_PW = "X-Loopers-LoginPw"
+    }
+}
