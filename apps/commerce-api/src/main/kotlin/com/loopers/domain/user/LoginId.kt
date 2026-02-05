76c488dfb34a3d8a7f9368db58baabaf8feca1fa
eoruadl <108312272+eoruadl@users.noreply.github.com>
2026-02-06 02:14:38 +0900

    feat: LoginId Value Object 구현
    - 4~20자 영문, 숫자 조합 검증
    - JPA AttributeConverter 구현

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/user/LoginId.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/user/LoginId.kt
@@ -1,6 +1,6 @@
 package com.loopers.domain.user
 
-data class LoginId (val value: String) {
+data class LoginId(val value: String) {
     init {
         require(value.matches(Regex("^[a-zA-Z0-9]+\$"))) {
             "LoginId는 영문과 숫자로만 구성합니다."
--- apps/commerce-api/src/test/kotlin/com/loopers/domain/user/LoginIdTest.kt
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/user/LoginIdTest.kt
@@ -7,32 +7,28 @@ import org.junit.jupiter.api.assertThrows
 class LoginIdTest {
 
     @Test
-    fun `영문과 숫자로만 이루어지면 성공한다` () {
-
+    fun `영문과 숫자로만 이루어지면 성공한다`() {
         assertDoesNotThrow {
             LoginId("test123")
         }
     }
 
     @Test
-    fun `빈 값인 경우 실패한다` () {
-
+    fun `빈 값인 경우 실패한다`() {
         assertThrows<IllegalArgumentException> {
             LoginId("")
         }
     }
 
     @Test
-    fun `공백이 있는 경우 실패한다` () {
-
+    fun `공백이 있는 경우 실패한다`() {
         assertThrows<IllegalArgumentException> {
             LoginId("test 123")
         }
     }
 
     @Test
-    fun `특수 문자가 있는 경우 실패한다` () {
-
+    fun `특수 문자가 있는 경우 실패한다`() {
         assertThrows<IllegalArgumentException> {
             LoginId("test123!@#")
         }
