286b5b22ac3caee133ee0bd3cd1fbc5019581613
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2026-02-06 12:31:40 +0900

    feat: 회원가입 Facade 구현

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/application/member/MemberFacade.kt
@@ -0,0 +1,26 @@
+package com.loopers.application.member
+
+import com.loopers.domain.member.MemberService
+import com.loopers.infrastructure.member.BCryptPasswordEncoder
+import com.loopers.interfaces.api.member.MemberV1Dto
+import org.springframework.stereotype.Component
+
+@Component
+class MemberFacade(
+    private val memberService: MemberService,
+    private val passwordEncoder: BCryptPasswordEncoder,
+) {
+    fun signUp(request: MemberV1Dto.SignUpRequest): MemberInfo {
+        val encodedPassword = passwordEncoder.encode(request.password)
+
+        val member = memberService.signUp(
+            loginId = request.loginId,
+            encodedPassword = encodedPassword,
+            name = request.name,
+            birthDate = request.birthDate,
+            email = request.email,
+        )
+
+        return MemberInfo.from(member)
+    }
+}
