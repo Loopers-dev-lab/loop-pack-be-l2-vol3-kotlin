cf855bdd504927e5d89d19d8bda3860ab6e1d572
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2026-02-06 14:56:15 +0900

    refactor: 비밀번호 검증 로직을 PasswordValidator 유틸리티로 추출
    SignUpRequest에 인라인되어 있던 비밀번호 검증 로직을 PasswordValidator 객체로 분리하여 재사용 가능하게 함

--- apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/member/MemberV1Dto.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/member/MemberV1Dto.kt
@@ -1,11 +1,9 @@
 package com.loopers.interfaces.api.member
 
 import com.loopers.application.member.MemberInfo
-import com.loopers.support.error.CoreException
-import com.loopers.support.error.ErrorType
 import com.loopers.support.util.MaskingUtils
+import com.loopers.support.util.PasswordValidator
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 class MemberV1Dto {
     data class SignUpRequest(
@@ -16,29 +14,7 @@ class MemberV1Dto {
         val email: String,
     ) {
         init {
-            validatePassword(password, birthDate)
-        }
-
-        private fun validatePassword(password: String, birthDate: LocalDate) {
-            if (password.length < 8 || password.length > 16) {
-                throw CoreException(ErrorType.BAD_REQUEST, "비밀번호는 8~16자여야 합니다.")
-            }
-            if (!password.matches(Regex("^[a-zA-Z0-9!@#\$%^&*()_+\\-=\\[\\]{}|;':\",./<>?]+$"))) {
-                throw CoreException(ErrorType.BAD_REQUEST, "비밀번호는 영문 대소문자, 숫자, 특수문자만 허용됩니다.")
-            }
-            if (containsBirthDate(password, birthDate)) {
-                throw CoreException(ErrorType.BAD_REQUEST, "비밀번호에 생년월일을 포함할 수 없습니다.")
-            }
-        }
-
-        private fun containsBirthDate(password: String, birthDate: LocalDate): Boolean {
-            val yyyymmdd = birthDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-            val yymmdd = birthDate.format(DateTimeFormatter.ofPattern("yyMMdd"))
-            val mmdd = birthDate.format(DateTimeFormatter.ofPattern("MMdd"))
-
-            return password.contains(yyyymmdd) ||
-                password.contains(yymmdd) ||
-                password.contains(mmdd)
+            PasswordValidator.validatePassword(password, birthDate, loginId)
         }
     }
 
