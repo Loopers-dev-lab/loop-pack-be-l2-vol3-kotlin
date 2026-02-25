1b5866e5bbfc5250fd5787094916f91952920907
tsseo <tsseo.dev@gmail.com>
2026-02-25 12:09:43 +0900

    feat: Brand 리포지토리 구현체 구현

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/brand/BrandRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.loopers.infrastructure.brand
+
+import com.loopers.domain.brand.Brand
+import com.loopers.domain.brand.BrandRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class BrandRepositoryImpl(
+    private val brandJpaRepository: BrandJpaRepository,
+) : BrandRepository {
+
+    override fun save(brand: Brand): Brand {
+        return brandJpaRepository.save(brand)
+    }
+
+    override fun findById(id: Long): Brand? {
+        return brandJpaRepository.findByIdOrNull(id)
+    }
+
+    override fun findAll(): List<Brand> {
+        return brandJpaRepository.findAll()
+    }
+
+    override fun findAll(pageable: Pageable): Page<Brand> {
+        return brandJpaRepository.findAllByDeletedAtIsNull(pageable)
+    }
+}
