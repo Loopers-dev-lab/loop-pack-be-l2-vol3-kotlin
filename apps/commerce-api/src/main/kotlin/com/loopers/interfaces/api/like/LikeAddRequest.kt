62e1c62fc2468546cf1af385201d8aa1fa8a1589
yewonahn <yewonahn0930@gmail.com>
2026-02-27 02:15:39 +0900

    test: Like API E2E 테스트 추가

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/like/LikeAddRequest.kt
@@ -0,0 +1,8 @@
+package com.loopers.interfaces.api.like
+
+import jakarta.validation.constraints.NotNull
+
+data class LikeAddRequest(
+    @field:NotNull(message = "상품 ID는 필수입니다.")
+    val productId: Long?,
+)
