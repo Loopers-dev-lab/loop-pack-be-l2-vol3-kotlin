4e8aaf11073ceac4e2b819ff450a2736d74e9f4a
tsseo <tsseo.dev@gmail.com>
2026-02-26 14:20:59 +0900

    feat: Admin 인증 헤더 상수 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/support/constant/HttpHeaders.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/support/constant/HttpHeaders.kt
@@ -3,4 +3,5 @@ package com.loopers.support.constant
 object HttpHeaders {
     const val LOGIN_ID = "X-Loopers-LoginId"
     const val LOGIN_PW = "X-Loopers-LoginPw"
+    const val LDAP = "X-Loopers-Ldap"
 }
