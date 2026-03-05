9dee4a7ed2597c878e3a16330eeda491ff514581
안유진 <anewjean00@gmail.com>
2026-03-05 18:35:38 +0900

    fix(UserLikeProduct): 불필요한 @Transactional 어노테이션 삭제

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/application/like/UserLikeProductUseCase.kt
@@ -0,0 +1,22 @@
+package com.loopers.application.like
+
+import com.loopers.application.UseCase
+import com.loopers.domain.catalog.ProductService
+import com.loopers.domain.like.LikeProductCommand
+import com.loopers.domain.like.ProductLikeService
+import com.loopers.domain.user.UserService
+import org.springframework.stereotype.Component
+
+@Component
+class UserLikeProductUseCase(
+    private val userService: UserService,
+    private val productService: ProductService,
+    private val productLikeService: ProductLikeService,
+) : UseCase<LikeProductCriteria, Unit> {
+
+    override fun execute(criteria: LikeProductCriteria) {
+        val user = userService.getUser(criteria.loginId)
+        productService.getProduct(criteria.productId)
+        productLikeService.like(LikeProductCommand(userId = user.id, productId = criteria.productId))
+    }
+}
